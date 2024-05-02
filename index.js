62fc43b19932a5b98be77e4d6f358831623029e6
Nishi-1805 <nishimishra180500@gmail.com>
2024-05-02 17:08:30 +0530

    ninthfile added

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-let mainHeading = document.querySelector('#main-heading');
-mainHeading.style.textAlign = 'center';
+const form = document.querySelector('form');
 
-const fruits= document.querySelector('.fruits');
-fruits.style.backgroundColor = 'gray';
-fruits.style.padding = '30px';
-fruits.style.margin = '30px';
-fruits.style.width = '50%';
-fruits.style.borderRadius = '5px';
-fruits.style.listStyleType = 'none';
+const fruits = document.querySelector('.fruits');
 
-const basketHeading = document.querySelector('h2');
-basketHeading.style.marginLeft = '30px';
-basketHeading.style.color = 'brown'
+form.addEventListener('submit', function(event){
+    event.preventDefault();
 
-const fruitItems = document.querySelectorAll('.fruit');
-for(let i=0; i<fruitItems.length; i++){
-  fruitItems[i].style.backgroundColor = 'white';
- fruitItems[i].style.padding = '10px';
- fruitItems[i].style.margin = '10px';
- fruitItems[i].style.borderRadius = '5px';
-}
-const OddfruitItems = document.querySelectorAll('.fruit:nth-child(odd)');
-for(let i=0; i<OddfruitItems.length; i++){
-  OddfruitItems[i].style.backgroundColor = 'lightgray';
-}
-const EvenfruitItems = document.querySelectorAll('.fruit:nth-child(even)');
-for(let i=0; i<OddfruitItems.length; i++){
-  EvenfruitItems[i].style.backgroundColor = 'brown';
-  EvenfruitItems[i].style.color = 'white';
-}
+    const inputValue = document.getElementById('fruit-to-add');
 
-// Write answer to the questions asked below:
+    //Li-adding
+    const newLi = document.createElement('li');
+    const newLitext = document.createTextNode(inputValue.value);
+ newLi.appendChild(newLitext);
+ newLi.className = 'fruit';
+
+//deltetBTN
+ const deleteBtn = document.createElement('button');
+ const deleteBtnText = document.createTextNode('x');
+ deleteBtn.appendChild(deleteBtnText);
+ deleteBtn.className = 'delete-btn';
+ newLi.appendChild(deleteBtn);
+
+
+
+
+//editBTN
+
+const editBtn = document.createElement('button');
+const editBtnText = document.createTextNode('Edit');
+ editBtn.appendChild(editBtnText);
+ editBtn.className = 'edit-btn';
+ newLi.appendChild(editBtn);
+
+
+//adding li as the last element of unorder list
+fruits.appendChild(newLi);
+
+
+});
+
+
+fruits.addEventListener('click', function(event){
+    if(event.target.classList.contains('delete-btn')){
+        const fruitToDelete = event.target.parentElement;
+        fruits.removeChild(fruitToDelete);
+    };
+    
+});
